4ecc1ea30798ebdf661c427b3966235ddb749351
phurit-d <phurit.de@gmail.com>
2025-08-13 00:02:42 +0700

    feat: add title to document head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>7Solutions Frontend Assignment</title>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
